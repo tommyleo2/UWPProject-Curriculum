e04a1b0026758834ee07098ada08e8538955ae9d
tommyleo2 <137581768@qq.com>
2016-05-14 16:30:33 +0800

    Update CurrentCurriculum.xaml.cs

--- UWPProject-Curriculum/UWPProject-Curriculum/CurrentCurriculum.xaml.cs
+++ UWPProject-Curriculum/UWPProject-Curriculum/CurrentCurriculum.xaml.cs
@@ -16,7 +16,6 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-<<<<<<< HEAD
 namespace UWPProject_Curriculum {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
